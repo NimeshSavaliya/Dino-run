0fd579ab3a4c5978132c0f6b84cbd99e6d2cdcd8
Nimesh Savaliya <nksavaliya3455@gmail.com>
2021-11-07 14:39:36 +0530

    obstacle genration and game over

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const grid = document.querySelector('.grid');
 const dino = document.querySelector('.dino');
+const body = document.querySelector('body');
 
 let isJumping = false;
-const gravity = 0.9;
+const gravity = 0.95;
+let isGameOver = false;
 
 const control = (e) => {
   if (e.keyCode === 32 || e.keyCode === 38) {
@@ -28,7 +30,7 @@ const jump = () => {
     count++;
     position = position * gravity;
     dino.style.bottom = position + 'px';
-    if (count === 25) {
+    if (count === 15) {
       clearInterval(upTimer);
     }
   }, 20);
@@ -45,3 +47,40 @@ const jump = () => {
     }
   }, 20);
 };
+
+//obstacle generation
+
+const generateObstacle = () => {
+  let randomTime = Math.random() * 4000;
+
+  let obstaclePosition = 0;
+  const obstacle = document.createElement('div');
+  {
+    !isGameOver && obstacle.classList.add('obstacle');
+  }
+
+  grid.appendChild(obstacle);
+  obstacle.style.right = obstaclePosition + 'px';
+
+  let timer = setInterval(() => {
+    if (position < 50 && obstaclePosition < 800 && obstaclePosition > 750) {
+      clearInterval(timer);
+      isGameOver = true;
+
+      //   body.removeChild(body.firstChild);
+      while (grid.firstChild) {
+        grid.removeChild(grid.lastChild);
+      }
+
+      grid.innerHTML = 'GAME OVER';
+    }
+
+    obstaclePosition += 7;
+    obstacle.style.right = obstaclePosition + 'px';
+  }, 20);
+
+  if (!isGameOver) {
+    setTimeout(generateObstacle, randomTime);
+  }
+};
+generateObstacle();
